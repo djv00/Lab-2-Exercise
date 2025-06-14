1f62581b1e242ff7adedebfd70f538ca5dbd28d9
Jiangyu Dai <dai00055@algonquincollege.com>
2025-06-14 15:53:49 -0400

    Done WhatIsNewJava9

--- src/main/java/com/algonquincollege/cst8277/week3/labexercise2/WhatIsNewJava9.java
+++ src/main/java/com/algonquincollege/cst8277/week3/labexercise2/WhatIsNewJava9.java
@@ -26,7 +26,7 @@ import org.slf4j.LoggerFactory;
  * Helper class that demonstrates some New features from Java 9
  *
  */
-public class WhatIsNewJava9 implements ImAnInterfaceWithADefaultMethod {
+public class WhatIsNewJava9 implements ImAnInterfaceWithNewDefaultMethods {
 	protected static final Class<?> MY_KLASSNAME = MethodHandles.lookup().lookupClass();
 	private static final Logger logger = LoggerFactory.getLogger(MY_KLASSNAME);
 
@@ -38,11 +38,11 @@ public class WhatIsNewJava9 implements ImAnInterfaceWithADefaultMethod {
 		ProcessHandle.Info processHandleInfo = processHandle.info();
 		// TODO - Change all info's to be initialized from processHandleInfo APIs
 		Optional<String[]> info1 = processHandleInfo.arguments();
-		Optional<String> info2 = Optional.of("Command");
-		Optional<String> info3 = Optional.of("CommandLine");
-		Optional<Instant> info4 = Optional.of(Instant.now());
-		Optional<Duration> info5 = Optional.of(Duration.ofMillis(1));
-		Optional<String> info6 = Optional.of("User");
+		Optional<String> info2 = processHandleInfo.command();
+		Optional<String> info3 = processHandleInfo.commandLine();
+		Optional<Instant> info4 = processHandleInfo.startInstant();
+		Optional<Duration> info5 = processHandleInfo.totalCpuDuration();
+		Optional<String> info6 = processHandleInfo.user();
 
 		DateTimeFormatter instantFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
 				.withZone(ZoneId.systemDefault());
@@ -51,12 +51,12 @@ public class WhatIsNewJava9 implements ImAnInterfaceWithADefaultMethod {
 		// TODO - Use 'get' method on Optional info variables; use 'orElse' method on Optional if it could be empty 
 		logger.info("pid = {}", pid);
 		logger.info("info1 = {}", Arrays.deepToString(info1.orElse(new String[]{"no", "arguments" })));
-		logger.info("info2 = {}", info2.get());
-		logger.info("info3 = {}", info3.get());
+		logger.info("info2 = {}", info2.orElse("<no command>"));
+		logger.info("info3 = {}", info3.orElse("<no commandLine>"));
 		//TODO - Use instantFormatter to format your date
 		logger.info("info4 = {}", info4Formatted);
-		logger.info("info5 = {} ms", info5.get().toMillis());
-		logger.info("info6 = {}", info6.get());
+		logger.info("info5 = {} ms", info5.map(Duration::toMillis).orElse(-1L));
+		logger.info("info6 = {}", info6.orElse("<no user>"));
 
 		//TODO - Change to use the new private static interface method.
 		//Do this by changing the interface to ImAnInterfaceWithNewDefaultMethods.
